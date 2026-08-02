c904483415e0e1c812dfb7bd7a22a3814c4f50a1
naz <naz@foid.itshim.org>
2026-08-02 23:14:13 +0300

    sdsd

---
+++ Content.Shared/_Mono/Capture/Components/CapturableComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Shared._Mono.Capture.Components;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class CapturableComponent : Component
+{
+    
+}
---
+++ Content.Shared/_Mono/Capture/Systems/CaptureSystem.cs
@@ -0,0 +1,13 @@
+namespace Content.Shared._Mono.Capture.Systems;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class CaptureSystem : EntitySystem
+{
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        
+    }
+}
